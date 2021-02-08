54b051658f44f0e6b8f54fb3a7eb3076fb1788a3
vergilinyan <dmitriy.karpeka@gmail.com>
2021-02-08 12:34:24 +0200

    very 1st

---
+++ 1st/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Display shit");
+        Log.w("MainActivity", "Warn your ass");
+        Log.e("MainActivity", "Except you from existence");
+    }
+}
